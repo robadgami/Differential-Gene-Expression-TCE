b0969ca3c8b7ca30a3751ffae08a2bb88735763d
robadgami <roshani.badgami@jic.ac.uk>
2019-08-01 09:39:20 +0100

    ggplot LFC to depict previously annotated genes.

--- ggplot_LFC_SA.R
+++ ggplot_LFC_SA.R
@@ -5,30 +5,31 @@ View(SA_F22v1314_LFC)
 colnames(SA_F22v1314_LFC)
 SA_F22v1314_LFC[is.na(SA_F22v1314_LFC)] <- 0
 
-
-
-SA_F22v1314_LFC_1dpi <- SA_F22v1314_LFC[SA_F22v1314_LFC$dpi == '1dpi',]
+library(ggplot2)
 ggplot(SA_F22v1314_LFC, aes(y=`log2FoldChange-F22v1314`, x=`log2FoldChange-SAvOA`, color=dpi, label=gene)) + geom_point(size=1.5) + theme_bw() +
   xlab('LFC for F22-infected Santiago vs Oakley') + ylab('LFC for Santiago infected by F22 vs 13/14') +
   geom_vline(xintercept=0, color='dark grey') + geom_hline(yintercept=0, color='dark grey')
 
 SA_F22v1314_LFC_1dpi <- SA_F22v1314_LFC[SA_F22v1314_LFC$dpi == '1dpi',]
 View(SA_F22v1314_LFC_1dpi)
-ggplot(SA_F22v1314_LFC_1dpi, aes(y=`log2FoldChange-F22v1314`, x=`log2FoldChange-SAvOA`, color=dpi, label=gene)) + geom_point(size=1.5,color='brown2') + theme_bw() +
+ggplot(SA_F22v1314_LFC_1dpi, aes(y=`log2FoldChange-F22v1314`, x=`log2FoldChange-SAvOA`, color=annotated, label=gene)) + geom_point(size=1.5) + theme_bw() +
   xlab('LFC for F22-infected Santiago vs Oakley') + ylab('LFC for Santiago infected by F22 vs 13/14') +
   geom_vline(xintercept=0, color='dark grey') + geom_hline(yintercept=0, color='dark grey') + ggtitle('1 dpi') +
-  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`> 0.5,as.character(gene),'')),hjust=0.5,vjust=1.5, size=3, angle=10,color='brown2') +
-  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`< -0.5,as.character(gene),'')),hjust=0.5,vjust=1.5, size=3, angle=10,color='brown2')
+  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`> 0.5,as.character(gene),'')),hjust=0.6,vjust=1.5, size=3, angle=10) +
+  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`< -0.5,as.character(gene),'')),hjust=0.6,vjust=1.5, size=3, angle=10)
 
 SA_F22v1314_LFC_3dpi <- SA_F22v1314_LFC[SA_F22v1314_LFC$dpi == '3dpi',]
 View(SA_F22v1314_LFC_3dpi)
-ggplot(SA_F22v1314_LFC_3dpi, aes(y=`log2FoldChange-F22v1314`, x=`log2FoldChange-SAvOA`, color=dpi, label=gene)) + geom_point(size=1.5,color='steelblue1') + theme_bw() +
+ggplot(SA_F22v1314_LFC_3dpi, aes(y=`log2FoldChange-F22v1314`, x=`log2FoldChange-SAvOA`, color=annotated, label=gene)) + geom_point(size=1.5) + theme_bw() +
   xlab('LFC for F22-infected Santiago vs Oakley') + ylab('LFC for Santiago infected by F22 vs 13/14') +
   geom_vline(xintercept=0, color='dark grey') + geom_hline(yintercept=0, color='dark grey') +ggtitle('3 dpi') +
-  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`> 1,as.character(gene),'')),hjust=0.5,vjust=1.5, size=3, angle=10,color='steelblue1') +
-  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`< -1,as.character(gene),'')),hjust=0.5,vjust=1.5, size=3, angle=10,color='steelblue1')
+  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`> 1,as.character(gene),'')),hjust=0.5,vjust=1.5, size=3, angle=10) +
+  geom_text(aes(label=ifelse(`log2FoldChange-SAvOA`> -1,as.character(gene),'')),hjust=0.5,vjust=1.5, size=3, angle=90)
+
+  geom_text(aes(label=ifelse(`log2FoldChange-F22v1314`< -1,as.character(gene),'')),hjust=0.5,vjust=1.5, size=3, angle=10)
 
 SA_F22v1314_LFC_7dpi <- SA_F22v1314_LFC[SA_F22v1314_LFC$dpi == '7dpi',]
+View(SA_F22v1314_LFC_7dpi)
 ggplot(SA_F22v1314_LFC_7dpi, aes(y=`log2FoldChange-F22v1314`, x=`log2FoldChange-SAvOA`, color=dpi, label=gene)) + geom_point(size=1.5,color='seagreen4') + theme_bw() +
   xlab('LFC for F22-infected Santiago vs Oakley') + ylab('LFC for Santiago infected by F22 vs 13/14') +
   geom_vline(xintercept=0, color='dark grey') + geom_hline(yintercept=0, color='dark grey') +ggtitle('7 dpi') +
